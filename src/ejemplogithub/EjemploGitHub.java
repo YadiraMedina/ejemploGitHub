253d34ea97b782f5d61ddf7e81fae32c479a0fca
Usuario <Usuario@Lenovo-PC>
2018-09-05 21:09:47 -0500

    Se inicia el proyecto.

---
+++ src/ejemplogithub/EjemploGitHub.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+Team --> Git --> Inicializar repositorio --> OK
+Crear repositorio en git web y copiar HTTPs
+proyecto--> clic derecho --> Git --> commit
+ */
+package ejemplogithub;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EjemploGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
